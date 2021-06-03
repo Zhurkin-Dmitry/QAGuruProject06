0d97ea605cf95ad728edf557c8b41bff66e63ece
Zhurkin-Dmitriy <zhurkindo@gmail.com>
2021-06-03 21:19:00 +0300

    Structure of project

---
+++ src/test/java/tests/GithubIssueTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class GithubIssueTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+
+    @Test
+    void issueSelenideTest() {
+
+    }
+
+    @Test
+    void issueLambdaTest() {
+
+    }
+
+    @Test
+    void issueWithAnnotationStep() {
+
+    }
+
+}
