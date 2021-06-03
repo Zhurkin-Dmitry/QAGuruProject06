e42d339972a0fe32a40dd658a389f938c750e205
Zhurkin-Dmitriy <zhurkindo@gmail.com>
2021-06-03 23:48:26 +0300

    Labda test

--- src/test/java/tests/GithubIssueTest.java
+++ src/test/java/tests/GithubIssueTest.java
@@ -1,10 +1,23 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GithubIssueTest {
+
+    String github = "https://github.com/",
+            headerSearch = ".header-search-input",
+            repository = "eroshenkoam/allure-example";
+    int issueNumber = 68;
 
     @BeforeAll
     static void setup() {
@@ -14,12 +27,35 @@ public class GithubIssueTest {
 
     @Test
     void issueSelenideTest() {
-
+        //SelenideLogger.addListener("allure", new AllureSelenide());
+        open(github);
+        $(headerSearch).click();
+        $(headerSearch).sendKeys(repository);
+        $(headerSearch).submit();
+        $(By.linkText(repository)).click();
+        $(withText("Issues")).click();
+        $(withText("#" + issueNumber)).should(Condition.exist);
     }
 
     @Test
     void issueLambdaTest() {
-
+        step("Открываем сайт Github", () -> {
+            open(github);
+        });
+        step("Поиск репозитория", () -> {
+            $(headerSearch).click();
+            $(headerSearch).sendKeys(repository);
+            $(headerSearch).submit();
+        });
+        step("Переходим к репозиторию", () -> {
+            $(By.linkText(repository)).click();
+        });
+        step("Открытие Issues", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Проверка существования Issue №68", () -> {
+            $(withText("#" + 68)).should(Condition.exist);
+        });
     }
 
     @Test
